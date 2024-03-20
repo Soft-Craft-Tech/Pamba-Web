3657f5c61b895f8fe9b0d4d81699e515e2ebc31c
regan-mu <regansomi@gmail.com>
2024-03-20 12:39:40 +0300

    Feature: Fetch a business's sales

--- API/sales/routes.py
+++ API/sales/routes.py
@@ -1,4 +1,4 @@
-from API.models import Sale, Service
+from API.models import Sale
 from flask import Blueprint, jsonify, request
 from API import db
 from API.lib.auth import business_login_required
@@ -13,12 +13,15 @@ def record_sale(business):
     """
         Record new Sales
         :param business: Business making the sale
-        :return: 200
+        :return: 200, 400
     """
     payload = request.get_json()
     payment_method = payload["paymentMethod"].strip()
     description = payload["description"].strip()
     service_id = payload["serviceId"]
+
+    if not business.active:
+        return jsonify({"message": "You need to activate your account first."}), 400
 
     business_services = [service.id for service in business.services.all()]
     if service_id not in business_services:
@@ -35,3 +38,23 @@ def record_sale(business):
     db.session.commit()
 
     return jsonify({"message": "Sale Added", "newSale": serialize_sale(sale)}), 200
+
+
+@sales_blueprint.route("/all", methods=["GET"])
+@business_login_required
+def fetch_all_business_sales(business):
+    """
+        Fetch all the sales for a given business
+        :param business: Business
+        :return:
+    """
+
+    sales = Sale.query.filter_by(business_id=business.id).all()
+    all_sales = []
+
+    for sale in sales:
+        sale_info = serialize_sale(sale)
+        sale_info["service"] = sale.service.service
+        all_sales.append(sale_info)
+
+    return jsonify({"message": "Sales", "sales": all_sales})
