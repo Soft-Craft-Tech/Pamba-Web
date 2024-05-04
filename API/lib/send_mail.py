387ce6be3d4f40326cd9008c0968f9e873433471
regan-mu <regansomi@gmail.com>
2024-05-04 13:31:39 +0300

    Fix verify Business Account URL

--- API/lib/send_mail.py
+++ API/lib/send_mail.py
@@ -51,7 +51,7 @@ def business_account_activation_email(recipient, token, name):
         :param name: Business Name.
         :return:
     """
-    url = f"https://www.pamba.africa/activate/{token}"
+    url = f"https://www.pamba.africa/verify/{token}"
     message = Message("[Action Required]: Activate your Pamba account", sender="pamba.africa", recipients=[recipient])
     message.html = render_template("activatebusiness.html", name=name, url=url)
     mail.send(message)
